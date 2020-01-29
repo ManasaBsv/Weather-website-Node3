17257742133c709d2e50bd4105f60c9a464c70f5
ManasaBsv <bsvmannu@gmail.com>
2020-01-29 20:07:54 +0530

    loading.. changes

--- public/js/app.js
+++ public/js/app.js
@@ -23,8 +23,11 @@ const messageTwo=document.querySelector('#message-2')
 weatherform.addEventListener('submit',(e)=>
 {   e.preventDefault() 
     const location=search.value
+    messageOne.textContent='Loading...'
+    messageTwo.textContent=''
     fetch('/weather?address='+location).then((response)=>
-    {
+    {    
+          
         response.json().then((data)=>
         {
                 if(data.error)
@@ -34,10 +37,10 @@ weatherform.addEventListener('submit',(e)=>
                 }
                 else
                 {
-                    messageOne.textContent=''
+                    messageOne.textContent=data.location
                     messageTwo.textContent= data.forecast 
                 }
-        })
+})
     })  
 
     //console.log(location)
