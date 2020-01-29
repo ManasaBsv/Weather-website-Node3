7179e713375c1d3affed5759e92f5b22fb5de90b
ManasaBsv <bsvmannu@gmail.com>
2020-01-29 20:24:32 +0530

    FROntend changes

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -15,7 +15,7 @@ const forecast=(latitude,longitude,callback)=>
         }
         else
         {   const dat=body
-            const s='It is currently '+dat.currently.temperature+' degrees farehnheit out. There is '+dat.currently.precipProbability+'% chance of rain'
+            const s=dat.daily.data[0].summary+' It is currently '+dat.currently.temperature+' degrees farehnheit out. There is '+dat.currently.precipProbability+'% chance of rain'
             callback(undefined,s)
         }
 
